7329f584f40c33ced7a28298f84022dcd39dd80c
Amadou Jamanka <ajamanka72@gmail.com>
2025-02-21 19:38:31 +0000

    update LoginForm and RegisterForm styles for better responsiveness

--- components/Form/LoginForm.jsx
+++ components/Form/LoginForm.jsx
@@ -85,7 +85,7 @@ const LoginForm = () => {
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className="text-center mb-4"
+            className="text-center mb-4 lg:text-start"
           >
             <Link
               href="/"
